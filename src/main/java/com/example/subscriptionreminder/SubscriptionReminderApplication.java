4872aea410e8c661f44d0db41c800d2ae6b8428f
Haruka Nakajima <haruca058729@gmail.com>
2026-09-10 13:32:04 -0400

    feat: initial project setup with Docker (MySQL + MailHog)

---
+++ src/main/java/com/example/subscriptionreminder/SubscriptionReminderApplication.java
@@ -0,0 +1,13 @@
+package com.example.subscriptionreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionReminderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionReminderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/subscriptionreminder/SubscriptionReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subscriptionreminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionReminderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
